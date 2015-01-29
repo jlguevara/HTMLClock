da6b1440c2943a7995b7f0926948864190656c71
jorge <jlguevara81@gmail.com>
2015-01-29 15:39:40 -0800

    using forcast api

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,8 @@
-getTime();
+$(document).ready(function() {
+        getTime();
+        getTemp();
+        });
+
 
 function getTime() {
     var d = new Date();
@@ -18,4 +22,34 @@ function getTime() {
     
     document.getElementById("clock").innerHTML = time; 
     setTimeout(getTime, 1000);
+}
+
+function getTemp() {
+    $.getJSON("https://api.forecast.io/forecast/946a91a7ec0e59c41c392a71c9a934b0/35.300399,-120.662362?callback=?", 
+            function(results) {
+            var temp = results["daily"]["data"][0]["temperatureMax"];
+            var className;
+
+            $("#forecastIcon").attr("src", "img/" + results["daily"]["data"][0]["icon"] + ".png");
+            $("#forecastLabel").html(results["daily"]["data"][0]["summary"]);
+
+            if (temp >= 90) {
+            className = "hot";
+            }
+            else if (temp >= 80) {
+            className = "warm";
+            }
+            else if (temp >= 70) {
+            className = "nice";
+            }
+            else if (temp >= 60) {
+            className = "chilly";
+            }
+            else {
+                className = "cold";
+            }
+            $("body").addClass(className);
+            }
+            );
+
 }
