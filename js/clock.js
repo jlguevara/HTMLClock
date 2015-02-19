35dc6b1f73be27237b77fbff45a58990ebce17e5
jorge <jlguevara81@gmail.com>
2015-02-19 11:15:57 -0800

    getting google username

--- js/clock.js
+++ js/clock.js
@@ -157,6 +157,14 @@ function getTemp() {
 function signinCallback(authResult) {
     if (authResult['status']['signed_in']) {
         document.getElementById('signinButton').setAttribute('style', 'display: none');
+        gapi.client.load('plus','v1', function(){
+                var request = gapi.client.plus.people.get({
+                    'userId': 'me'
+                    });
+                request.execute(function(resp) {
+                    console.log('Retrieved profile for:' + resp.displayName);
+                    });
+                });
     } else {
         console.log('Sign-in state: ' + authResult['error']);
     }
